6f3ad5d8ed17019a6600d24c9bd43fd5d94432fb
Jeremy Bowman <jbowman@edx.org>
2016-12-14 14:47:03 -0500

    PLAT-1104 Import courses asynchronously

--- cms/djangoapps/contentstore/views/import_export.py
+++ cms/djangoapps/contentstore/views/import_export.py
@@ -11,9 +11,13 @@ import tarfile
 from path import Path as path
 from tempfile import mkdtemp
 
+from six import text_type
+
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
-from django.core.exceptions import SuspiciousOperation, PermissionDenied
+from django.core.exceptions import PermissionDenied
+from django.core.files import File
+from django.core.files.storage import default_storage
 from django.core.files.temp import NamedTemporaryFile
 from django.core.servers.basehttp import FileWrapper
 from django.http import HttpResponse, HttpResponseNotFound, Http404
@@ -21,27 +25,20 @@ from django.utils.translation import ugettext as _
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.views.decorators.http import require_http_methods, require_GET
 
-import dogstats_wrapper as dog_stats_api
 from edxmako.shortcuts import render_to_response
 from xmodule.contentstore.django import contentstore
 from xmodule.exceptions import SerializationError
 from xmodule.modulestore.django import modulestore
 from opaque_keys.edx.keys import CourseKey
 from opaque_keys.edx.locator import LibraryLocator
-from xmodule.modulestore.xml_importer import import_course_from_xml, import_library_from_xml
+from user_tasks.models import UserTaskStatus
 from xmodule.modulestore.xml_exporter import export_course_to_xml, export_library_to_xml
-from xmodule.modulestore import COURSE_ROOT, LIBRARY_ROOT
 
 from student.auth import has_course_author_access
 
-from openedx.core.lib.extract_tar import safetar_extractall
 from util.json_request import JsonResponse
 from util.views import ensure_valid_course_key
-from models.settings.course_metadata import CourseMetadata
-from contentstore.views.entrance_exam import (
-    add_entrance_exam_milestone,
-    remove_entrance_exam_milestone_reference
-)
+from contentstore.tasks import CourseImportTask, import_olx
 
 from contentstore.utils import reverse_course_url, reverse_usage_url, reverse_library_url
 
@@ -76,23 +73,19 @@ def import_handler(request, course_key_string):
     courselike_key = CourseKey.from_string(course_key_string)
     library = isinstance(courselike_key, LibraryLocator)
     if library:
-        root_name = LIBRARY_ROOT
         successful_url = reverse_library_url('library_handler', courselike_key)
         context_name = 'context_library'
         courselike_module = modulestore().get_library(courselike_key)
-        import_func = import_library_from_xml
     else:
-        root_name = COURSE_ROOT
         successful_url = reverse_course_url('course_handler', courselike_key)
         context_name = 'context_course'
         courselike_module = modulestore().get_course(courselike_key)
-        import_func = import_course_from_xml
     return _import_handler(
-        request, courselike_key, root_name, successful_url, context_name, courselike_module, import_func
+        request, courselike_key, successful_url, context_name, courselike_module
     )
 
 
-def _import_handler(request, courselike_key, root_name, successful_url, context_name, courselike_module, import_func):
+def _import_handler(request, courselike_key, successful_url, context_name, courselike_module):
     """
     Parameterized function containing the meat of import_handler.
     """
@@ -103,6 +96,7 @@ def _import_handler(request, courselike_key, root_name, successful_url, context_
         if request.method == 'GET':
             raise NotImplementedError('coming soon')
         else:
+            # Upload .tar.gz to local filesystem for one-server installations not using S3 or Swift
             # Do everything in a try-except block to make sure everything is properly cleaned up.
             try:
                 data_root = path(settings.GITHUB_REPO_ROOT)
@@ -111,19 +105,8 @@ def _import_handler(request, courselike_key, root_name, successful_url, context_
                 filename = request.FILES['course-data'].name
 
                 # Use sessions to keep info about import progress
-                session_status = request.session.setdefault("import_status", {})
-                courselike_string = unicode(courselike_key) + filename
+                courselike_string = text_type(courselike_key) + filename
                 _save_request_status(request, courselike_string, 0)
-
-                # If the course has an entrance exam then remove it and its corresponding milestone.
-                # current course state before import.
-                if root_name == COURSE_ROOT:
-                    if courselike_module.entrance_exam_enabled:
-                        remove_entrance_exam_milestone_reference(request, courselike_key)
-                        log.info(
-                            "entrance exam milestone content reference for course %s has been removed",
-                            courselike_module.id
-                        )
 
                 if not filename.endswith('.tar.gz'):
                     _save_request_status(request, courselike_string, -1)
@@ -195,6 +178,14 @@ def _import_handler(request, courselike_key, root_name, successful_url, context_
                             "thumbnailUrl": ""
                         }]
                     })
+
+                log.info("Course import %s: Upload complete", courselike_key)
+                with open(temp_filepath, 'rb') as local_file:
+                    django_file = File(local_file)
+                    storage_path = default_storage.save(u'olx_import/' + filename, django_file)
+                import_olx.delay(
+                    request.user.id, text_type(courselike_key), storage_path, filename, request.LANGUAGE_CODE)
+
             # Send errors to client with stage at which error occurred.
             except Exception as exception:  # pylint: disable=broad-except
                 _save_request_status(request, courselike_string, -1)
@@ -213,125 +204,7 @@ def _import_handler(request, courselike_key, root_name, successful_url, context_
                     status=400
                 )
 
-            # try-finally block for proper clean up after receiving last chunk.
-            try:
-                # This was the last chunk.
-                log.info("Course import %s: Upload complete", courselike_key)
-                _save_request_status(request, courselike_string, 1)
-
-                tar_file = tarfile.open(temp_filepath)
-                try:
-                    safetar_extractall(tar_file, (course_dir + '/').encode('utf-8'))
-                except SuspiciousOperation as exc:
-                    _save_request_status(request, courselike_string, -1)
-                    return JsonResponse(
-                        {
-                            'ErrMsg': 'Unsafe tar file. Aborting import.',
-                            'SuspiciousFileOperationMsg': exc.args[0],
-                            'Stage': -1
-                        },
-                        status=400
-                    )
-                finally:
-                    tar_file.close()
-
-                log.info("Course import %s: Uploaded file extracted", courselike_key)
-                _save_request_status(request, courselike_string, 2)
-
-                # find the 'course.xml' file
-                def get_all_files(directory):
-                    """
-                    For each file in the directory, yield a 2-tuple of (file-name,
-                    directory-path)
-                    """
-                    for dirpath, _dirnames, filenames in os.walk(directory):
-                        for filename in filenames:
-                            yield (filename, dirpath)
-
-                def get_dir_for_fname(directory, filename):
-                    """
-                    Returns the dirpath for the first file found in the directory
-                    with the given name.  If there is no file in the directory with
-                    the specified name, return None.
-                    """
-                    for fname, dirpath in get_all_files(directory):
-                        if fname == filename:
-                            return dirpath
-                    return None
-
-                dirpath = get_dir_for_fname(course_dir, root_name)
-                if not dirpath:
-                    _save_request_status(request, courselike_string, -2)
-                    return JsonResponse(
-                        {
-                            'ErrMsg': _('Could not find the {0} file in the package.').format(root_name),
-                            'Stage': -2
-                        },
-                        status=415
-                    )
-
-                dirpath = os.path.relpath(dirpath, data_root)
-                logging.debug('found %s at %s', root_name, dirpath)
-
-                log.info("Course import %s: Extracted file verified", courselike_key)
-                _save_request_status(request, courselike_string, 3)
-
-                with dog_stats_api.timer(
-                    'courselike_import.time',
-                    tags=[u"courselike:{}".format(courselike_key)]
-                ):
-                    courselike_items = import_func(
-                        modulestore(), request.user.id,
-                        settings.GITHUB_REPO_ROOT, [dirpath],
-                        load_error_modules=False,
-                        static_content_store=contentstore(),
-                        target_id=courselike_key
-                    )
-
-                new_location = courselike_items[0].location
-                logging.debug('new course at %s', new_location)
-
-                log.info("Course import %s: Course import successful", courselike_key)
-                _save_request_status(request, courselike_string, 4)
-
-            # Send errors to client with stage at which error occurred.
-            except Exception as exception:   # pylint: disable=broad-except
-                log.exception(
-                    "error importing course"
-                )
-                return JsonResponse(
-                    {
-                        'ErrMsg': str(exception),
-                        'Stage': -session_status[courselike_string]
-                    },
-                    status=400
-                )
-
-            finally:
-                if course_dir.isdir():
-                    shutil.rmtree(course_dir)
-                    log.info("Course import %s: Temp data cleared", courselike_key)
-                # set failed stage number with negative sign in case of unsuccessful import
-                if session_status[courselike_string] != 4:
-                    _save_request_status(request, courselike_string, -abs(session_status[courselike_string]))
-
-                # status == 4 represents that course has been imported successfully.
-                if session_status[courselike_string] == 4 and root_name == COURSE_ROOT:
-                    # Reload the course so we have the latest state
-                    course = modulestore().get_course(courselike_key)
-                    if course.entrance_exam_enabled:
-                        entrance_exam_chapter = modulestore().get_items(
-                            course.id,
-                            qualifiers={'category': 'chapter'},
-                            settings={'is_entrance_exam': True}
-                        )[0]
-
-                        metadata = {'entrance_exam_id': unicode(entrance_exam_chapter.location)}
-                        CourseMetadata.update_from_dict(metadata, course, request.user)
-                        add_entrance_exam_milestone(course.id, entrance_exam_chapter)
-                        log.info("Course %s Entrance exam imported", course.id)
-
-            return JsonResponse({'Status': 'OK'})
+            return JsonResponse({'ImportStatus': 1})
     elif request.method == 'GET':  # assume html
         status_url = reverse_course_url(
             "import_status_handler", courselike_key, kwargs={'filename': "fillerName"}
@@ -368,9 +241,9 @@ def import_status_handler(request, course_key_string, filename=None):
 
         -X : Import unsuccessful due to some error with X as stage [0-3]
         0 : No status info found (import done or upload still in progress)
-        1 : Extracting file
-        2 : Validating.
-        3 : Importing to mongo
+        1 : Unpacking
+        2 : Verifying
+        3 : Updating
         4 : Import successful
 
     """
@@ -379,10 +252,23 @@ def import_status_handler(request, course_key_string, filename=None):
         raise PermissionDenied()
 
     try:
-        session_status = request.session["import_status"]
-        status = session_status[course_key_string + filename]
-    except KeyError:
-        status = 0
+        # The task status record is authoritative once it's been created
+        args = {u'course_key_string': course_key_string, u'archive_name': filename}
+        name = CourseImportTask.generate_name(args)
+        task_status = UserTaskStatus.objects.filter(name=name).order_by(u'-created').first()
+        if task_status.state == UserTaskStatus.SUCCEEDED:
+            status = 4
+        elif task_status.state in (UserTaskStatus.FAILED, UserTaskStatus.CANCELED):
+            status = max(-(task_status.completed_steps + 1), -3)
+        else:
+            status = min(task_status.completed_steps + 1, 3)
+    except UserTaskStatus.DoesNotExist:
+        # The task hasn't been initialized yet; did we store info in the session already?
+        try:
+            session_status = request.session["import_status"]
+            status = session_status[course_key_string + filename]
+        except KeyError:
+            status = 0
 
     return JsonResponse({"ImportStatus": status})
 
