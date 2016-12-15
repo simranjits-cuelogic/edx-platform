513b56ea51b57ec91f399b5c72a8336cd0bb1c17
Brian Jacobel <bjacobel@gmail.com>
2016-12-15 16:16:03 -0500

    Convert both files to coffeescript, preserving comments

--- common/lib/xmodule/xmodule/js/src/sequence/edit.js
+++ common/lib/xmodule/xmodule/js/src/sequence/edit.js
@@ -1,2 +1,19 @@
-class @SequenceDescriptor extends XModule.Descriptor
+/* globals XModule */
+/* eslint-disable no-underscore-dangle */
 
+(function() {
+    'use strict';
+
+    var __hasProp = {}.hasOwnProperty,
+        __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; }; // eslint-disable-line
+
+    this.SequenceDescriptor = (function(_super) {
+        __extends(SequenceDescriptor, _super);
+
+        function SequenceDescriptor() {
+            return SequenceDescriptor.__super__.constructor.apply(this, arguments);
+        }
+
+        return SequenceDescriptor;
+    }(XModule.Descriptor));
+}).call(this);
