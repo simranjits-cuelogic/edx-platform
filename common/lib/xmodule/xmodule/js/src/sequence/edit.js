f5045a26b34c47f384c10958e5ac8a5a0bd0c594
Brian Jacobel <bjacobel@gmail.com>
2016-12-16 09:53:14 -0500

    Clean up generated js

--- common/lib/xmodule/xmodule/js/src/sequence/edit.js
+++ common/lib/xmodule/xmodule/js/src/sequence/edit.js
@@ -8,11 +8,11 @@
         __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; }; // eslint-disable-line
 
     this.SequenceDescriptor = (function(_super) {
-        __extends(SequenceDescriptor, _super);
-
         function SequenceDescriptor() {
             return SequenceDescriptor.__super__.constructor.apply(this, arguments);
         }
+
+        __extends(SequenceDescriptor, _super);
 
         return SequenceDescriptor;
     }(XModule.Descriptor));
